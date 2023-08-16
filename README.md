385b90cb2f9e8f15b5f433f480dfe53ea40cb9f1
I2rys <i2rys@protonmail.com>
2023-08-16 20:34:32 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# Disolated
+Login to your Discord account anonymously with a token.
+
+## Installation
+Github:
+```
+git clone https://github.com/cspi-git/disolated
+```
+
+NpmJS:
+```
+npm i electron
+```
+
+## Usage
+Please run the **tor.exe** on the Tor folder first then wait for the bootstrap to be on 100% before running:
+```
+node index.js <token>
+```
+
+- token - Your Discord account token.
+
+## License
+MIT © CSPI
---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// Dependencies
+const { app, BrowserWindow, session } = require("electron")
+
+// Variables
+const args = process.argv.slice(2)
+const blocked = ["https://discord.com/error-reporting-proxy/web"]
+
+// Main
+if(!args.length) return console.log("usage: node index.js <token>")
+
+app.on("ready", ()=>{
+    app.commandLine.appendSwitch("proxy-server", "socks5://127.0.0.1:9050")
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            sandbox: true,
+            enableRemoteModule: false
+        }
+    })
+    mainWindow.loadURL("https://discord.com/channels/@me")
+
+    mainWindow.webContents.executeJavaScript(`
+setInterval(()=>{
+    document.body.appendChild(document.createElement \`iframe\`).contentWindow.localStorage.token = \`"${args[0]}"\`
+}, 50)
+
+setTimeout(() => {location.reload()}, 2500)`)
+
+    session.defaultSession.webRequest.onBeforeSendHeaders((details, callback)=>{
+        if(blocked.includes(details.url)) return callback({ cancel: true })
+
+        delete details.requestHeaders["User-Agent"]
+        delete details.requestHeaders["sec-ch-ua"]
+        delete details.requestHeaders["sec-ch-ua-platform"]
+        callback({ cancel: false, requestHeaders: details.requestHeaders })
+    })
+})
